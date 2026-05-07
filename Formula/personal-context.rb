6dd41847ceedf34657976297be92eed92c09314d
Nick Conn <350756+nicholasjconn@users.noreply.github.com>
2026-05-07 20:40:30 +0000

    personal-context: add 0.1.0 bottle.

--- Formula/personal-context.rb
+++ Formula/personal-context.rb
@@ -5,6 +5,12 @@ class PersonalContext < Formula
   sha256 "b6aceca47ac09eaa0399633678a5d61a09e00bc29866387cb5bc86911b16b487"
   license "PolyForm-Noncommercial-1.0.0"
 
+  bottle do
+    root_url "https://github.com/conn-castle/homebrew-tap/releases/download/personal-context-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "2243d8808c68c914837047d6a9a2e37d28fc6fe76041ffba8234c895d9b91904"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "dd9a112cc50b65702b9cd63e8e9005dff4579e5fd0d8842b7ee9dafad54318f0"
+  end
+
   depends_on "go" => :build
 
   def install
